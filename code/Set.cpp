c1674b16657365fdc8c7d0c99b0ad4b2ca2c62a0
MordovinNik <mordovin_nikolya@mail.ru>
2021-02-07 00:07:11 +0300

    добавлены функции

--- code/Set.cpp
+++ code/Set.cpp
@@ -14,6 +14,14 @@ SetElement::SetElement()
     prev = next = nullptr;
 }
 
+SetElement::SetElement(const SetElement &element)
+{
+    memcpy(data, element.data, strlen(element.data) + 1);
+
+    next = nullptr;
+    prev = nullptr;
+}
+
 SetElement::~SetElement()
 {
     next = prev = nullptr;
@@ -52,7 +60,7 @@ const char *SetElement::Data() const
     return data;
 }
 
-bool operator<(SetElement left, SetElement right)
+bool operator<(const SetElement &left, const SetElement &right)
 {
     int i = 0;
 
@@ -62,7 +70,7 @@ bool operator<(SetElement left, SetElement right)
     return left.data[i] < right.data[i];
 }
 
-bool operator<=(SetElement left, SetElement right)
+bool operator<=(const SetElement &left, const SetElement &right)
 {
     int i = 0;
 
@@ -72,9 +80,12 @@ bool operator<=(SetElement left, SetElement right)
     return left.data[i] <= right.data[i];
 }
 
-bool operator==(SetElement left, SetElement right)
+bool operator==(const SetElement &left, const SetElement &right)
 {
     int i = 0;
+
+    if (&left == &right)
+        return true;
 
     while (i < 80 && left.data[i] != 0 && right.data[i] != 0 && left.data[i] == right.data[i])
         i++;
@@ -82,7 +93,7 @@ bool operator==(SetElement left, SetElement right)
     return left.data[i] == right.data[i];
 }
 
-bool operator>(SetElement left, SetElement right)
+bool operator>(const SetElement &left, const SetElement &right)
 {
     int i = 0;
 
@@ -167,7 +178,7 @@ std::ostream &operator<<(std::ostream &out, const MySet &set)
     return out;
 }
 
-MySet::MySet() : list(nullptr), cardinality(0){}
+MySet::MySet() : list(nullptr), cardinality(0) {}
 
 SetElement *MySet::FindElement(const char *value) const
 {
@@ -208,35 +219,196 @@ int MySet::DeleteElement(const char *value)
 
     if (tmp == list)
         list = tmp->next;
-    
+
     cardinality--;
     delete tmp;
     return 0;
 }
 
-int MySet::Cardinality()const
+int MySet::Cardinality() const
 {
     return cardinality;
 }
 
+int MySet::AddElement(SetElement element)
+{
+    return AddElement(element.Data());
+}
+
+MySet::MySet(const MySet &src)
+{
+    cardinality = 0;
+    list = nullptr;
+    SetElement *tmp = src.list;
+
+    while (tmp->next != src.list)
+    {
+        AddElement(*tmp);
+        tmp = tmp->next;
+    }
+    AddElement(*tmp);
+}
+
+MySet &Unite(const MySet &Set1, const MySet &Set2)
+{
+    MySet *newSet = new MySet(Set1);
+    SetElement *tmp = Set2.list;
+
+    while (tmp->Next() != Set2.list)
+    {
+        newSet->AddElement(*tmp);
+        tmp = tmp->Next();
+    }
+
+    newSet->AddElement(*tmp);
+
+    return *newSet;
+}
+
+MySet &Intersection(const MySet &Set1, const MySet &Set2)
+{
+    MySet *newSet = new MySet;
+    SetElement *element1 = Set1.list, *element2 = Set2.list;
+
+    while (true)
+    {
+        if (*element1 < *element2)
+        {
+            element1 = element1->Next();
+        }
+        else if (*element1 == *element2)
+        {
+            newSet->AddElement(*element2);
+            element1 = element1->Next();
+            element2 = element2->Next();
+        }
+        else
+        {
+            element2 = element2->Next();
+        }
+        if (element1 == Set1.list || element2 == Set2.list)
+            break;
+    }
+    return *newSet;
+}
+
+MySet &Difference(const MySet &Set1, const MySet &Set2)
+{
+    MySet *newSet = new MySet;
+    SetElement *element1 = Set1.list, *element2 = Set2.list;
+
+    while (true)
+    {
+        if (*element1 < *element2)
+        {
+            newSet->AddElement(*element1);
+            element1 = element1->Next();
+        }
+        else if (*element1 == *element2)
+        {
+            element1 = element1->Next();
+            element2 = element2->Next();
+        }
+        else
+        {
+            element2 = element2->Next();
+        }
+
+        if (element1 == Set1.list)
+            break;
+
+        if (element2 == Set2.list)
+        {
+            while (element1 != Set1.list)
+            {
+                newSet->AddElement(*element1);
+                element1 = element1->Next();
+            }
+            break;
+        }
+    }
+    return *newSet;
+}
+
+MySet &SymDiff(const MySet &Set1, const MySet &Set2)
+{
+    MySet *newSet = new MySet;
+    SetElement *element1 = Set1.list, *element2 = Set2.list;
+
+    while (true)
+    {
+        if (*element1 < *element2)
+        {
+            newSet->AddElement(*element1);
+            element1 = element1->Next();
+        }
+        else if (*element1 == *element2)
+        {
+            element1 = element1->Next();
+            element2 = element2->Next();
+        }
+        else
+        {
+            newSet->AddElement(*element2);
+            element2 = element2->Next();
+        }
+
+        if (element1 == Set1.list)
+        {
+            while (element2 != Set2.list)
+            {
+                newSet->AddElement(*element2);
+                element2 = element2->Next();
+            }
+            break;
+        }
+
+        if (element2 == Set2.list)
+        {
+            while (element1 != Set1.list)
+            {
+                newSet->AddElement(*element1);
+                element1 = element1->Next();
+            }
+            break;
+        }
+    }
+    return *newSet;
+}
+
+
 int main()
 {
-    MySet a;
-    a.AddElement("e3");
-    a.AddElement("b");
-    a.AddElement("aa");
-    a.AddElement("asdfas");
-    a.AddElement("fddgfsh");
-    a.DeleteElement("aa");
-    a.DeleteElement("b");
-    a.DeleteElement("e3");
-    a.DeleteElement("asdfas");
-    a.DeleteElement("fddgfsh");
-    a.AddElement("e3");
-    a.AddElement("b");
-    a.AddElement("aa");
-    a.AddElement("asdfas");
-    a.AddElement("fddgfsh");
+    MySet a, b;
 
-    cout << a;
+    a.AddElement("aa");
+    a.AddElement("bb");
+    a.AddElement("zz");
+    a.AddElement("dd");
+    a.AddElement("ee");
+
+    cout << a.Cardinality();
+
+    b.AddElement("aa");
+    b.AddElement("cc");
+    b.AddElement("gg");
+    b.AddElement("ff");
+    b.AddElement("uu");
+    cout << b.Cardinality();
+
+    cout << Difference(b, a);
+
+    //cout << a;
+    // a.DeleteElement("aa");
+    // a.DeleteElement("b");
+    // a.DeleteElement("e3");
+    // a.DeleteElement("asdfas");
+    // a.DeleteElement("fddgfsh");
+    // cout << a.AddElement("e3");
+    // cout << a.AddElement("b");
+    // cout << a.AddElement("aa");
+    // cout << a.AddElement("asdfas");
+    // cout << a.AddElement("fddgfsh");
+
+    //cout << a;
 }
--- code/Set.h
+++ code/Set.h
@@ -8,15 +8,16 @@ private:
 
 public:
     SetElement();
+    SetElement(const SetElement&);
     ~SetElement();
     int SetData(const char *dat);
     const char *Data() const;
     SetElement *Next();
 
-    friend bool operator<(SetElement, SetElement);
-    friend bool operator<=(SetElement,SetElement);
-    friend bool operator>(SetElement, SetElement);
-    friend bool operator==(SetElement, SetElement);
+    friend bool operator<(const SetElement &left, const SetElement &right);
+    friend bool operator<=(const SetElement &left, const SetElement &right);
+    friend bool operator>(const SetElement &left, const SetElement &right);
+    friend bool operator==(const SetElement &left, const SetElement& right);
     friend std::ostream &operator<<(std::ostream &out, const SetElement &element);
     friend class MySet;
 };
@@ -29,11 +30,17 @@ private:
 
 public:
     MySet();
+    MySet(const MySet&);
     int AddElement(const char *value);
+    int AddElement(SetElement);
     SetElement *FindElement(const char *value) const;
     int DeleteElement(const char *value);
     int Cardinality()const;
+    bool IsSubset(const MySet& src)const;
 
     friend std::ostream &operator<<(std::ostream &out, const MySet &set);
-    friend MySet Unite (MySet Set1,MySet Set2);
+    friend MySet& Unite (const MySet& Set1,const MySet& Set2);
+    friend MySet& Intersection(const MySet& Set1,const MySet& Set2);
+    friend MySet& Difference(const MySet &Set1, const MySet &Set2);
+    friend MySet &SymDiff(const MySet &Set1, const MySet &Set2);
 };
